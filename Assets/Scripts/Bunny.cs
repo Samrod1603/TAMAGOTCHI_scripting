2452d26ddfe47d9958a190f06b5b8fc44c5b7ce9
abrahamdaguer <68035021+abrahamdaguer@users.noreply.github.com>
2021-09-24 14:59:34 -0500

    Pets script (#2)

---
+++ Assets/Scripts/Bunny.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bunny : Pets
+{
+    public override void Names()
+    {
+        print("Hops");
+        
+    }
+}
